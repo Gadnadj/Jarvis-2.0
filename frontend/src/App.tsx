836d0c1f7d44979be073ffc875905112ef87c6e0
Gad Nadjar <gadnadjar@gmail.com>
2024-08-05 11:40:31 +0300

    add mute audio

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Controller from './components/Controller';
 import Deaf from './components/Deaf';
 import Mute from './components/Mute';
+import MuteAudio from './components/MuteAudio';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path='/' element={<Controller />} />
         <Route path='/deaf' element={<Deaf />} />
         <Route path='/mute' element={<Mute />} />
+        <Route path='/mute-audio' element={<MuteAudio />} />
       </Routes>
     </Router>
   );
